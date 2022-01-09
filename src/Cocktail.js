5ac7df8e261553ee437e20a6679c087ac73868b5
Lior Makovsky <liormakovsky@gmail.com>
2022-01-09 22:33:14 +0200

    start cocktail project

---
+++ src/Cocktail.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import { API_ENDPOINT } from './context'
+
+const Cocktail = () => {
+  return <h2>single cocktail</h2>
+}
+
+export default Cocktail
---
+++ src/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Form from './SearchForm'
+import Cocktails from './Cocktails.js'
+const Home = () => {
+  return <h2>cocktail component</h2>
+}
+
+export default Home
